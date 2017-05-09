e1fafa03888e2c0eae98734f321f4329723e465c
gretanderson <tuf82795@temple.edu>
2017-05-09 12:17:27 -0400

    Add files via upload

---
+++ greta-reverse-engineering-4/states.js
@@ -0,0 +1,218 @@
+$(document).ready(function() {
+		$(".state-groups").click(function(){$(".state-groups.active").removeClass("active"); 
+			$(this).addClass("active"); 
+		});
+
+		$(".nationwide-btn").click(function() {$(".card").hide(); 
+			$("#national").show(); 
+			$(".state-groups.active").removeClass("active");
+		});
+
+		$("#national").on('click', function() {$(".card").hide();
+			$("#nationwide-btn").show();
+		});
+
+		$("#ME").on('click', function() {$(".card").hide();
+			$("#card-ME").show();
+		});
+
+		$("#WI").on('click', function() {$(".card").hide();
+			$("#card-WI").show();
+		});
+
+		$("#VT").on('click', function() {$(".card").hide();
+			$("#card-VT").show();
+		});
+
+		$("#NH").on('click', function() {$(".card").hide();
+			$("#card-NH").show();
+		});
+
+		$("#WA").on('click', function() {$(".card").hide();
+			$("#card-WA").show();
+		});
+
+		$("#ID").on('click', function() {$(".card").hide();
+			$("#card-ID").show();
+		});
+
+		$("#MT").on('click', function() {$(".card").hide();
+			$("#card-MT").show();
+		});
+
+		$("#ND").on('click', function() {$(".card").hide();
+			$("#card-ND").show();
+		});
+
+		$("#SD").on('click', function() {$(".card").hide();
+			$("#card-SD").show();
+		});
+
+		$("#MN").on('click', function() {$(".card").hide();
+			$("#card-MN").show();
+		});
+
+		$("#MI").on('click', function() {$(".card").hide();
+			$("#card-MI").show();
+		});
+
+		$("#IL").on('click', function() {$(".card").hide();
+			$("#card-IL").show();
+		});
+
+		$("#NY").on('click', function() {$(".card").hide();
+			$("#card-NY").show();
+		});
+
+		$("#MO").on('click', function() {$(".card").hide();
+			$("#card-MO").show();
+		});
+
+		$("#OR").on('click', function() {$(".card").hide();
+			$("#card-OR").show();
+		});
+
+		$("#NV").on('click', function() {$(".card").hide();
+			$("#card-NV").show();
+		});
+
+		$("#WY").on('click', function() {$(".card").hide();
+			$("#card-WY").show();
+		});
+
+		$("#IA").on('click', function() {$(".card").hide();
+			$("#card-IA").show();
+		});		
+
+		$("#IN").on('click', function() {$(".card").hide();
+			$("#card-IN").show();
+		});
+		
+		$("#OH").on('click', function() {$(".card").hide();
+			$("#card-OH").show();
+		});
+
+		$("#PA").on('click', function() {$(".card").hide();
+			$("#card-PA").show();
+		});
+		
+		$("#NJ").on('click', function() {$(".card").hide();
+			$("#card-NJ").show();
+		});
+
+		$("#CT").on('click', function() {$(".card").hide();
+			$("#card-CT").show();
+		});
+
+		$("#RI").on('click', function() {$(".card").hide();
+			$("#card-RI").show();
+		});
+
+		$("#CA").on('click', function() {$(".card").hide();
+			$("#card-CA").show();
+		});
+
+		$("#UT").on('click', function() {$(".card").hide();
+			$("#card-UT").show();
+		});
+
+		$("#CO").on('click', function() {$(".card").hide();
+			$("#card-CO").show();
+		});
+
+		$("#NE").on('click', function() {$(".card").hide();
+			$("#card-NE").show();
+		});
+
+		$("#MI").on('click', function() {$(".card").hide();
+			$("#card-MI").show();
+		});
+		
+		$("#KY").on('click', function() {$(".card").hide();
+			$("#card-KY").show();
+		});
+
+		$("#WV").on('click', function() {$(".card").hide();
+			$("#card-WV").show();
+		});
+
+		$("#VA").on('click', function() {$(".card").hide();
+			$("#card-VA").show();
+		});
+
+		$("#MD").on('click', function() {$(".card").hide();
+			$("#card-MD").show();
+		});
+
+		$("#DE").on('click', function() {$(".card").hide();
+			$("#card-DE").show();
+		});
+
+		$("#NM").on('click', function() {$(".card").hide();
+			$("#card-NM").show();
+		});
+
+		$("#AZ").on('click', function() {$(".card").hide();
+			$("#card-AZ").show();
+		});
+
+		$("#KS").on('click', function() {$(".card").hide();
+			$("#card-KS").show();
+		});
+
+		$("#AR").on('click', function() {$(".card").hide();
+			$("#card-AR").show();
+		});
+
+		$("#TN").on('click', function() {$(".card").hide();
+			$("#card-TN").show();
+		});
+
+		$("#NC").on('click', function() {$(".card").hide();
+			$("#card-NC").show();
+		});
+
+		$("#SC").on('click', function() {$(".card").hide();
+			$("#card-SC").show();
+		});
+
+		$("#DC").on('click', function() {$(".card").hide();
+			$("#card-DC").show();
+		});
+
+		$("#OK").on('click', function() {$(".card").hide();
+			$("#card-OK").show();
+		});
+
+		$("#LA").on('click', function() {$(".card").hide();
+			$("#card-LA").show();
+		});
+
+		$("#MS").on('click', function() {$(".card").hide();
+			$("#card-MS").show();
+		});
+
+		$("#AL").on('click', function() {$(".card").hide();
+			$("#card-AL").show();
+		});
+
+		$("#AR").on('click', function() {$(".card").hide();
+			$("#card-AR").show();
+		});
+
+		$("#GA").on('click', function() {$(".card").hide();
+			$("#card-GA").show();
+		});
+
+		$("#HI").on('click', function() {$(".card").hide();
+			$("#card-HI").show();
+		});
+
+		$("#TX").on('click', function() {$(".card").hide();
+			$("#card-TX").show();
+		});
+
+		$("#FL").on('click', function() {$(".card").hide();
+			$("#card-FL").show();
+		});
+});
